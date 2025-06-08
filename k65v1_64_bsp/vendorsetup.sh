3f7d954b188971b89dce8394435ca1925717e270
abdmeh11 <30433370+abdmeh11@users.noreply.github.com>
2025-06-08 20:06:49 +0300

    Update vendorsetup.sh

--- k65v1_64_bsp/vendorsetup.sh
+++ k65v1_64_bsp/vendorsetup.sh
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-add_lunch_combo omni_k65v1_64_bsp-user
-add_lunch_combo omni_k65v1_64_bsp-userdebug
-add_lunch_combo omni_k65v1_64_bsp-eng
+add_lunch_combo twrp_k65v1_64_bsp-user
+add_lunch_combo twrp_k65v1_64_bsp-userdebug
+add_lunch_combo twrp_k65v1_64_bsp-eng
 
