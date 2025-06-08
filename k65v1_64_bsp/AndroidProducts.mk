2fef63dfc6900df330619a37e26e3d16af612c7a
abdmeh11 <30433370+abdmeh11@users.noreply.github.com>
2025-06-08 20:11:56 +0300

    Update AndroidProducts.mk

--- k65v1_64_bsp/AndroidProducts.mk
+++ k65v1_64_bsp/AndroidProducts.mk
@@ -7,7 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_k65v1_64_bsp.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_k65v1_64_bsp-user \
-    omni_k65v1_64_bsp-userdebug \
-    omni_k65v1_64_bsp-eng
+    twrp_k65v1_64_bsp-user \
+    twrp_k65v1_64_bsp-userdebug \
+    twrp_k65v1_64_bsp-eng
 
